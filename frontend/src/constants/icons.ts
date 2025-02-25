df6fe72364d84b07f7fccd143901b25ec8287869
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2025-02-26 00:11:45 +0100

    Add new asset -add new icons -add logo -define css constants

--- frontend/src/constants/icons.ts
+++ frontend/src/constants/icons.ts
@@ -4,6 +4,24 @@ import googleIcon from '../assets/icons/google.png';
 import hideIcon from '../assets/icons/hide.png';
 import showIcon from '../assets/icons/show.png';
 import idCardIcon from '../assets/icons/id-card.png';
+import homeIcon from '../assets/icons/home.png';
+import calendarIcon from '../assets/icons/calendar.png';
+import customersIcon from '../assets/icons/customers.png';
+import hammerIcons from '../assets/icons/hammer.png';
+import invoiceIcon from '../assets/icons/invoice.png';
+import helpIcon from '../assets/icons/help.png';
+import notificationIcon from '../assets/icons/notification.png';
+import settingsIcon from '../assets/icons/setting.png';
+import closeIcon from '../assets/icons/close.png';
+import exportIcon from '../assets/icons/export.png';
+import importIcon from '../assets/icons/import.png';
+import searchIcon from '../assets/icons/search.png';
+import sortIcon from '../assets/icons/sort.png';
+import filterIcon from '../assets/icons/filter.png';
+import nextPageIcon from '../assets/icons/next_page.png';
+import firstPageIcon from '../assets/icons/first-page.png';
+import lastPageIcon from '../assets/icons/last-page.png';
+import customerDetailsIcon from '../assets/icons/customer-details.png';
 
 export default {
   mailIcon,
@@ -12,4 +30,22 @@ export default {
   hideIcon,
   showIcon,
   idCardIcon,
+  homeIcon,
+  calendarIcon,
+  customersIcon,
+  hammerIcons,
+  invoiceIcon,
+  helpIcon,
+  notificationIcon,
+  settingsIcon,
+  closeIcon,
+  exportIcon,
+  importIcon,
+  searchIcon,
+  sortIcon,
+  filterIcon,
+  nextPageIcon,
+  firstPageIcon,
+  lastPageIcon,
+  customerDetailsIcon,
 };
---
+++ frontend/src/constants/images.ts
@@ -0,0 +1,5 @@
+import logo from '../assets/images/fieldsynchub_logo.svg';
+
+export default {
+  logo,
+};
