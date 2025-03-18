24b853c875d75df5f994cce9bddb2ab6a388a092
Faris010 <doricfaris50@gmail.com>
2025-03-18 17:18:16 +0100

    customer_model_update

--- backend/Models/Customers.cs
+++ backend/Models/Customers.cs
@@ -14,23 +14,11 @@ public class Customers
     public string? CompanyName { get; set; }
     public bool IsCompany { get; set; }
     public List<string>? Email { get; set; }
-    public string? Address { get; set; }
-    public string? City { get; set; }
-    public string? State { get; set; }
-    public string? Country { get; set; }
-    public int PostalCode { get; set; }
-    public string? BillingStreet { get; set; }
-    public string? BillingCity { get; set; }
-    public string? BillingProvince { get; set; }
-    public string? BillingPostalCode { get; set; }
-    public string? BillingCountry { get; set; }
-    public bool TextMessagesEnabled { get; set; }
     public bool VisitReminders { get; set; }
     public bool JobFollowUps { get; set; }
     public bool QuoteFollowUps { get; set; }
     public bool InvoiceFollowUps { get; set; }
     public bool Archived { get; set; }
-    public string? Note { get; set; }
     public string? Tags { get; set; }
     public ICollection<CustomFields>? CustomFields {get; set;}
     public ICollection<Notes>? Notes { get; set; }
