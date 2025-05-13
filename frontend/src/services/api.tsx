7ea7964197c66d89eb18d9edc81d5b9b76ed5683
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2025-05-13 16:24:02 +0200

    add api authorization check

--- frontend/src/services/api.tsx
+++ frontend/src/services/api.tsx
@@ -5,4 +5,53 @@ const instance = axios.create({
   withCredentials: true,
 });
 
-export default instance
+const refreshAccessToken = async () => {
+  try {
+    const response = await axios.post(
+      `${import.meta.env.VITE_BASE_URL}/auth/refresh`,
+      null,
+      {
+        withCredentials: true,
+      }
+    );
+    const { accessToken } = response.data;
+
+    localStorage.setItem('accessToken', accessToken);
+    return accessToken;
+  } catch (error) {
+    console.error('Failed to refresh access token:', error);
+    return null;
+  }
+};
+
+instance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const newAccessToken = await refreshAccessToken();
+
+      if (newAccessToken) {
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        return instance(originalRequest);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default instance;
